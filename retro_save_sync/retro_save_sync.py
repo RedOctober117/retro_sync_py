a2e87fb4dc3715f20f549657d4962c1f2252dfa7
RedOctober117 <dbcahilly3@gmail.com>
2024-01-12 22:07:36 -0500

    initial work on modification comparison

--- retro_save_sync/retro_save_sync.py
+++ retro_save_sync/retro_save_sync.py
@@ -12,6 +12,7 @@ save = open(SAVE_FOLDER + SAVE_FILE_NAME, 'rb')
 ss0 = open(SAVE_FOLDER + SS0_FILE_NAME, 'rb')
 
 
+dir_contents = None
 
 ftp = FTP()
 ftp.connect(HOST, PORT)
@@ -26,4 +27,8 @@ with ftp as ftp:
     ftp.storbinary('STOR ' + SAVE_FILE_NAME, save)
     ftp.storbinary('STOR ' + SS0_FILE_NAME, ss0)
 
-    ftp.dir()
+    # Fact reference: https://datatracker.ietf.org/doc/html/rfc3659.html#section-7.5
+    dir_contents = dict(ftp.mlsd(facts=['modify']))
+
+if SAVE_FILE_NAME in dir_contents:
+    print(dir_contents[SAVE_FILE_NAME])
